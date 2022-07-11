88f490fbe984de0197d3e63ce98e7d500f718d82
Leonard Roman <thelionestor@gmail.com>
2022-07-11 13:42:49 +0300

    1211

--- code.js
+++ code.js
@@ -4,14 +4,8 @@ function execute_code(){
   function alert_one(){ alert('one'); }
   function alert_two(){ alert('two'); }
   
-  var buyBtn = document.getElementsByClassName('product-form__submit');
-
-  for ( const buy_btn in buyBtn){
-    buy_btn.addEventListener('click', function (e) {
-      alert('123')
-    });
-  }
-
+  alert(1)
+  
 }
 
 execute_code();
