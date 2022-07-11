cc5d6c5c56abdedd4404861073613a5d22c58a78
Leonard Roman <thelionestor@gmail.com>
2022-07-11 13:21:18 +0300

    121

--- code.js
+++ code.js
@@ -4,7 +4,7 @@ function execute_code(){
   function alert_one(){ alert('one'); }
   function alert_two(){ alert('two'); }
    
-  document.getElementsByClassName("product-form__submit").addEventListener('click', function(e){
+  document.guerySelectors(".product-form__submit").addEventListener('click', function(e){
     alert('123')
   });
 
